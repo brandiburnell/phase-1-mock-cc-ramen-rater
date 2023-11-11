b918ef7e526a6e38b7797640b57825ae540f19d6
Brandi Burnell <brandiburnell6@gmail.com>
2023-11-11 09:53:11 -0700

    inital fetch for ramen menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // write your code here
+
+// Wait for DOM content to load before fetching
+document.addEventListener("DOMContentLoaded", () => {
+
+    // load ramen menu
+    loadRamenMenu();
+
+
+});
+
+function loadRamenMenu() {
+    // fetch ramen data from API
+    fetch("http://localhost:3000/ramens")
+        .then(res => res.json())
+        .then(ramens => {console.log(ramens)});
+}
